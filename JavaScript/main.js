e737772e91aaa84a4d97c37c9081299d0e7443b0
Weiquan Zeng <s4406382@student.uq.edu.au>
2020-09-26 17:47:05 +0800

    autoslideshower

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,11 +1,42 @@
-$.getJSON('https://apps.des.qld.gov.au/species/?op=getclassnames&kingdom=animals', function(data) {
-    var data = $(data.Family)
+// $.getJSON('https://apps.des.qld.gov.au/species/?op=getclassnames&kingdom=animals', function(data) {
+//     var data = $(data.Family)
         
-    var text = `Date: ${JSON.stringify(data.Family)}<br>
-                Time: ${data.time}<br>
-                Unix time: ${data.milliseconds_since_epoch}`
+//     var text = `Date: ${JSON.stringify(data.Family)}<br>
+//                 Time: ${data.time}<br>
+//                 Unix time: ${data.milliseconds_since_epoch}`
                 
     
-    $(".mypanel").html(text);
-    console.log(data)
-});
+//     $(".mypanel").html(text);
+//     console.log(data)
+// });
+
+
+
+// automatic slideshower
+// This code is inspired by an example found at https://www.w3schools.com/howto/howto_js_slideshow.as
+
+var index = 0;
+autoSlider();
+
+function autoSlider() {
+  var i;
+  var slides = document.getElementsByClassName("autoslider");
+  var time = 4500;
+
+  // iterate the length of slide and display as none
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  index++;
+
+  // when the index of slide larger than the total number slides set the index as 1 which equal to 
+  // the first slide
+  if (index > slides.length) {
+    index = 1
+  }
+
+  // show the silde 
+  slides[index-1].style.display = "block";
+
+  setTimeout(autoSlider, time); // duration of slide change
+}
