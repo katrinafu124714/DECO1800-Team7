2e4e898f224626c9a072ff56f43fe093b2cb048e
Yingxuan Li (Benson) <yingxuan.li1@uqconnect.edu.au>
2020-09-27 13:53:51 +1000

    deleted js folder

--- JS/api.js
+++
@@ -1,9 +0,0 @@
-const  api_url = "https://apps.des.qld.gov.au/species/"
-
-async function getData() {
-    const response = await fetch(api_url);
-    const data = await response.json();
-    console.log(data);
-}
-
-getData();
