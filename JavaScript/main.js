c81fcb8f37199d8266779a7927825fe7bbb97d3d
Weiquan Zeng <s4406382@student.uq.edu.au>
2020-09-29 01:30:27 +0800

    added new html files and updated the video page and added the slide controller for learn page

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -13,8 +13,36 @@
 // automatic slideshow
 // This code is inspired by an example found at https://www.w3schools.com/howto/howto_js_slideshow.as
 
-var index = 0;
+var index = 1;
+slideControler1(index);
 autoSlider();
+
+function slidesControler(value) {
+  slideControler1(index += value)
+}
+
+
+function slideControler1(value) {
+  var i;
+  var slides = document.getElementsByClassName("autoslider");
+
+  if (value < 1) {
+    index = slides.length
+  }
+  
+  if (value > slides.length) {
+    index = 1
+  }
+
+  // iterate the length of slide and display as none
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+
+  // show the silde 
+  slides[index-1].style.display = "block";
+}
+
 
 function autoSlider() {
   var i;
