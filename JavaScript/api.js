f14d88255aa82546241aa24b54be1ec1ac20f83e
Yingxuan Li (Benson) <yingxuan.li1@uqconnect.edu.au>
2020-10-04 21:52:27 +1000

    deleted splide lib

--- JavaScript/api.js
+++ JavaScript/api.js
@@ -1,20 +1,41 @@
 
+// var os = require('os')
+// console.log(JSON.stringify(os.EOL))
+var br=document.createElement('br');
+
 async function fetchWildNetData(species) {
     let url =`https://apps.des.qld.gov.au/species/?f=json&op=speciessearch&kingdom=animals&species=${species}`;
     const response = await fetch(url)
     const data = await response.json();
+
+    const content = "TaxonID: " + data.Species[0].TaxonID + "\n"
+    + data.Species[0].ScientificName + "\n" 
+    + data.Species[0].ConservationStatus.NCAStatus + "\n"
+    + data.Species[0].AcceptedCommonName + " is "
+    + data.Species[0].ClassCommonName
     
     console.log(data.Species[0])
-      var koala = document.getElementById("koala");
-      koala.innerHTML = "TaxonID: " + data.Species[0].TaxonID + " " 
-      + data.Species[0].ScientificName + " " 
-      + data.Species[0].ConservationStatus.NCAStatus + " "
-      + data.Species[0].AcceptedCommonName + " "
-      + data.Species[0].ClassCommonName
+      var specie = document.getElementById(species)
+      const info = document.createElement("div");
+      const text = document.createTextNode(content)
+      info.appendChild(text)
     
-    return data
-}
-//   var koala = document.getElementById("kola");
-//   koala.innerHTML = fetchWildNetData(koala)
 
-  fetchWildNetData("koala");
+      console.log(content)
+
+      specie.innerText = "TaxonID: " + data.Species[0].TaxonID + "\n"
+      + data.Species[0].ScientificName + "\n" 
+      + data.Species[0].ConservationStatus.NCAStatus + "\n"
+      + data.Species[0].AcceptedCommonName + " is "
+      + data.Species[0].ClassCommonName
+      
+    // return data
+}
+      var koala = document.getElementById("koala");
+
+
+fetchWildNetData("koala");
+
+fetchWildNetData("wombat")
+
+fetchWildNetData("wallaby")
