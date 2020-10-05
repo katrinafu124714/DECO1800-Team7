a861d3cbbf5c23296d02db89546fb8afe0b44d50
Yingxuan Li (Benson) <yingxuan.li1@uqconnect.edu.au>
2020-10-05 17:24:56 +1000

    php api

--- api.php
+++ api.php
@@ -1,12 +1,8 @@
 <?php 
-    // $koalaURL = `https://apps.des.qld.gov.au/species/?f=json&op=speciessearch&kingdom=animals&species=860`;
-    // $data = file_get_contents($koalaURL);
-    // $data = json_decode($data, true);
-    // echo '<pre>' . print_r($data, true) . '</pre>';
-
-
-    $api_url = 'https://www.data.qld.gov.au/api/3/action/datastore_search?resource_id=9eaeeceb-e8e3-49a1-928a-4df76b059c2d&limit=1000';
-    $data = file_get_contents($api_url);
+    // include("header.php");
+    $koalaURL = 'https://apps.des.qld.gov.au//species//?op=getspeciesinformation&taxonid=860';
+    $data = file_get_contents($koalaURL);
     $data = json_decode($data, true);
+    echo $data;
     echo '<pre>' . print_r($data, true) . '</pre>';
 ?>
