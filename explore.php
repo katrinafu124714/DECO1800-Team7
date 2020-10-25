c0e70046bbc8e9336ea98caab91721a3a5a390f0
cheng-web-bne <c.jiang.uq@gmail.com>
2020-10-26 01:37:06 +1000

    Implemented search function for map

--- explore.php
+++ explore.php
@@ -14,18 +14,53 @@
 <body>
   <!-- head -->
   <?php $page = "explore";include('header.php');?>
-  <div style="background-image: url('images/sea_background.jpg');
-  background-position: center;">
-  <article id="map" style="
-    position: relative; 
-    height: 500pt;
-    width: 80%;
-    align-self: center;
-    display: inline-block;
-    margin:10%;
-    ">
-  </div>
-  </article>
+
+    <div style="
+    background-image: url('images/sea_background.jpg');
+    background-position: center;">
+      <div class="container">
+         <h3 id="animal-search" style="
+            width: 100%;
+            height: 30px;
+            padding: 20px;
+            font-size: 30px;
+            align-item: center;
+            ">
+          Search Animals
+         </h3>
+        <input type="text" id="search" placeholder="Enter Species Name" style="
+          width: 100%;
+          height: 30px;
+          border-radius: 5px;
+          border: 5px solid #bce826;
+          padding: 10px;
+          font-size: 20px;
+          position: relative;
+          ">
+        <button onclick="handleInput()" id="searchButton" style="
+          border-radius: 5px;
+          background-color: #bce826;
+          padding: 10px;
+          font-size: 20px;
+          position: absolute;
+          height: 50px;
+          width: 120px;
+          top: 482px;
+          left: 86%;
+          "
+        >Show Map</button>
+        </div>
+        <article id="map" style="
+          position: relative; 
+          height: 500pt;
+          width: 80%;
+          align-self: center;
+          display: inline-block;
+          margin:10%;
+          margin-top: 2%;
+          ">
+        </article>
+    </div>
   <?php include("footer.php") ?>
 
 
