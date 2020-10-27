0f8bf00dfc4c21f4db5a963c79bf7b8c8f42daa8
Yingxuan Li (Benson) <yingxuan.li1@uqconnect.edu.au>
2020-10-28 03:48:06 +1000

    endless commit 2, fixed footer.php, include footer in the index.php

--- index.php
+++ index.php
@@ -120,22 +120,8 @@
   </section>
 
 <!-- ---------------------------------------------------------------- -->
-<footer>
-  <section>
-    <a href="about.html">About us</a>
-    <a href="">Join us</a>
-    <a href="">Subscribe</a>
-  </section>
-  <section>
-    <a href="about.html">Follow us on</a>
-    <a href="">Fackbook</a>
-    <a href="">Instagram</a>
-    <a href="">Twitter</a>
-  </section>
-  <section>
-    <img src="images/Logo.png">
-  </section>
-</footer>
+<?php include('footer.php') ?>
+
 <script src="JavaScript/main.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </body>
