e936508619eb5326d819d6e73316158bc4881630
katrinafu124714 <73165252+katrinafu124714@users.noreply.github.com>
2020-10-28 01:27:10 +0800

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -7,7 +7,125 @@
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <title>Wildlife Guide Junior - Learn</title>
 </head>
-
+<body>
 <?php include('header.php')?>
 <br>
 <h1>References</h1>
+<p>Australian reed warbler cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/QpxvK7KgZpqrjWBWA</p>
+<p></p>
+<p>Australian smelt. (2017). [Photograph]. https://images.app.goo.gl/HKqhqwBJw5HP6ptB6</p>
+<p></p>
+<p>Australian smelt cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/V6Di2dZLT9yXAf93A</p>
+<p></p>
+<p>Amphibians. (2019). [Photograph]. https://unsplash.com/photos/UdZkaHPxY4M</p>
+<p></p>
+<p>Australian bass cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/8Df1qTPs5JEPYBQDA</p>
+<p></p>
+<p>Balachandran, S. (2019). Birds [Photograph]. https://unsplash.com/photos/hXttDVCwyRA</p>
+<p></p>
+<p>Balachandran, S. (2018). Fish [Photograph]. https://unsplash.com/photos/YV593oyMKmo</p>
+<p></p>
+<p>Barramundi. (n.d.). [Photograph]. https://images.app.goo.gl/hCGf36fQa5gDnDQv9</p>
+<p></p>
+<p>Barramundi cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/AL574vYZqE8cf9is9</p>
+<p></p>
+<p>Boggess snail. (n.d.). [Photograph]. https://images.app.goo.gl/1B8WHvka9pBqoLBh9</p>
+<p></p>
+<p>Boggess snail cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/cruddiy1Jzr9z81Z6</p>
+<p></p>
+<p>Buchanan, A. (2020). yellow wall background [Photograph]. https://unsplash.com/photos/pVMhOC7HnzQ</p>
+<p></p>
+<p>Cairns birdwing. (2019). [Photograph]. https://unsplash.com/photos/Oq9JrJb6K7M</p>
+<p></p>
+<p>Cane toad. (2018). [Photograph]. https://unsplash.com/photos/v5AG4QUfyx8</p>
+<p></p>
+<p>Cane toad cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/peUERfUKSShqmwii6</p>
+<p></p>
+<p>Cairns birdwing cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/o8TpuJspkFaFRJB96</p>
+<p></p>
+<p>Common death adder. (n.d.).  [Photograph]. https://images.app.goo.gl/TpyiVnF7BroYusWM6</p>
+<p></p>
+<p>Common death adder cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/1BZXBNpjnxxn7NzM6</p>
+<p></p>
+<p>Chameleon gecko. (2018). [Photograph]. https://unsplash.com/photos/ktwf6kExFAw</p>
+<p></p>
+<p>Chameleon gecko cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/ufTs8i7eThWLBKEy9</p>
+<p></p>
+<p>Desert Scorpion. (2018). [Photograph]. https://images.app.goo.gl/f9pgRqxTLDmF8AXv7</p>
+<p></p>
+<p>Desert Scorpion cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/z1pLYKKMCMRJK4Vq5</p>
+<p></p>
+<p>Dingo cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/7APZ8grvFwnioLPR9</p>
+<p></p>
+<p>Eastern water dragon. (2018). [Photograph]. https://unsplash.com/photos/wuudw8YAoDg</p>
+<p></p>
+<p>Eastern water dragon cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/irgWbTsGy73XYNk1A</p>
+<p></p>
+<p>Freshwater crocodile. (2018). [Photograph]. https://unsplash.com/photos/Uuzq37W5HXs</p>
+<p></p>
+<p>Freshwater crocodile cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/y7rQEZ4wg6iTzWDs9</p>
+<p></p>
+<p>Green snail. (2018). [Photograph]. https://unsplash.com/photos/lUD9KmRnxDE</p>
+<p></p>
+<p>H, L. (2020). Giraffe and her calf [Photograph]. https://unsplash.com/photos/gOWuRBY7gDM</p>
+<p></p>
+<p>Hennessy, R. (2018). Australian reed warbler [Photograph]. https://unsplash.com/photos/w-xLR4q5MXM</p>
+<p></p>
+<p>Humpback cartoon. (n.d.). [Cartoon image]. https://www.freepik.com/free-vector/humpback-whale-nature-scene-silhouette_10139136.htm#page=1&query=cartoon+humpback&position=28</p>
+<p></p>
+<p>Kangaroo cartoon. (n.d.). [Cartoon image]. https://www.freepik.com/free-vector/illustration-drawing-style-set-wildlife_3129739.htm#page=1&query=cartoon%20kangaroo&position=14</p>
+<p></p>
+<p>Koala icon. (2020). [Icon]. https://www.flaticon.com/free-icon/koala_3069172</p>
+<p></p>
+<p>Kinnear, S. P. (2018). Mammal [Photograph]. https://unsplash.com/photos/FQjmQgSoRyQ</p>
+<p></p>
+<p>Kuwamura, K. (2018). white wall background [Photograph]. https://unsplash.com/photos/LzgK6IMoSZM</p>
+<p></p>
+<p>Mammal background. (2018). [Photograph]. https://unsplash.com/photos/Nk088TzYUlI</p>
+<p></p>
+<p>Morrison, N. (2017). Laptop and notepad [Photograph]. https://unsplash.com/photos/FHnnjk1Yj7Y</p>
+<p></p>
+<p>Pacific black duck. (n.d.). [Photograph]. https://images.app.goo.gl/q7cnEB2AWBsV7JfcA</p>
+<p></p>
+<p>Park, T. (2020). [Kindergarten boy looking at laptop computer during first day of virtual learning online school.]. https://unsplash.com/photos/6MePtA9EVDA</p>
+<p></p>
+<p>Possum cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/ruaq2j1M6pKFZoCk9</p>
+<p></p>
+<p>Richmond birdwing butterfly. (n.d.). [Photograph]. https://images.app.goo.gl/t5H6YhJhUcMi6JF36</p>
+<p></p>
+<p>Richmond birdwing butterfly cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/hptDYEFgGEBsNQuH7</p>
+<p></p>
+<p>R. (2018). Reptiles [Photograph]. https://unsplash.com/photos/Qx18LzMEAlY</p>
+<p></p>
+<p>Saw shelled turtle. (2018). [Photograph]. https://unsplash.com/photos/beUxN2FSt74</p>
+<p></p>
+<p>Saw shelled turtle cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/EdYFH2VW6bbYzJ6L7</p>
+<p></p>
+<p>Sea background. (2019). [Photograph]. https://unsplash.com/photos/NbPJ8HM-w8c</p>
+<p></p>
+<p></p>
+<p>Smooth stingray. (2018). [Photograph]. https://unsplash.com/photos/HaxrCVb1FEE</p>
+<p></p>
+<p>Smooth stingray cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/TTsgc4o97N8a2E227</p>
+<p></p>
+<p>Silky shark. (2018). [Photograph]. https://unsplash.com/photos/GBDkr3k96DE</p>
+<p></p>
+<p>Silky shark cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/BZYj9PuY88Li4sjJA</p>
+<p></p>
+<p>Torres, D. (2018). Invertebrates [Photograph]. https://unsplash.com/photos/OIl9uwiy10Q</p>
+<p></p>
+<p>Tree frog. (2018). [Photograph]. https://unsplash.com/photos/OZJdNV99BgU</p>
+<p></p>
+<p>Tree frog cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/zuJwkwitjyoZ7m7v9</p>
+<p></p>
+<p>Wallum sedge frog. (n.d.). [Photograph]. https://images.app.goo.gl/mEbyaJ372MrFs274A</p>
+<p></p>
+<p>Wallum sedge frog cartoon. (n.d.). [Cartoon image]. https://images.app.goo.gl/8XKogzHsgVgv4oHP7</p>
+<p></p>
+<p>White, B. (n.d.). Boy sitting on the bench [Photograph]. https://unsplash.com/photos/4K2lIP0zc_k</p>
+<p></p>
+<p>Wallaby cartoon. (n.d.). [Cartoon image]. https://www.freepik.com/premium-vector/cute-kangaroo-mascot-vector-design_6407553.htm</p>
+<p></p>
+<p>Yurasits, B. (2019). Australian bass [Photograph]. https://unsplash.com/photos/891v7kZMSMg</p>
+  
+</body>
