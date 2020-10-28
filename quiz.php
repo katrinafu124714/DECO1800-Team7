2fb543586510d9fc2580c73d9da3f68ca829ea9b
FeiHuo46100047 <71127031+FeiHuo46100047@users.noreply.github.com>
2020-10-28 12:57:40 +1000

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -13,32 +13,65 @@ include('header.php');
 ?>
 
 <body>
-<section id = "quiz_section">
-  <section class="quiz_content">
-    <h1>Question1</h1>
+  <section id = "quiz_section">
+    <div class = "quiz_content">
+      
+      <h1>Question1</h1>
+            
+      <h2>Drag the blow animal icon to the corresponding box</h2>
+            
+        <!-- this code are refer to https://codepen.io/Coding_Journey/pen/YzKpLvE -->
+      <section class="draggable-elements">
+        <i class="fas fa-cat draggable" draggable="true" id="cat"></i>
+        <i class="fas fa-dog draggable" draggable="true" id="dog"></i>
+        <i class="fas fa-dove draggable" draggable="true" id="dove"></i>
+        <i class="fas fa-fish draggable" draggable="true" id="fish"></i>
+        <i class="fas fa-frog draggable" draggable="true" id="frog"></i>
+      </section>
+            
+                <!-- this code are refer to https://codepen.io/Coding_Journey/pen/YzKpLvE -->
+      <section class="droppable-elements">
+        <div class="droppable" data-draggable-id="frog"><span>Frog</span></div>
+        <div class="droppable" data-draggable-id="dove"><span>Bird</span></div>
+        <div class="droppable" data-draggable-id="cat"><span>Cat</span></div>
+        <div class="droppable" data-draggable-id="fish"><span>Fish</span></div>
+        <div class="droppable" data-draggable-id="dog"><span>Dog</span></div>
+      </section>
+    </div>
 
-    <h2>Drag the blow animal icon to the corresponding box</h2>
+    <div class = "quiz_content">
+      <h1>Question2</h1>                         
+      <h2>What animal is in the picture below?</h2>
+      <div class="image_of_quiz"><img src="images/koala.jpg"></div>
+      <button class="short_options" type="button" onclick="notCorrect()";>A. Cat</button>
+      <button class="short_options" type="button" onclick="notCorrect()";>B. Dog</button>
+      <button class="short_options" type="button" onclick="notCorrect()";>C. Monkey</button>
+      <button class="short_options" type="button" onclick="correct()";>D. koala</button>
+    </div>
 
-    <!-- this code are refer to https://codepen.io/Coding_Journey/pen/YzKpLvE -->
-    <section class="draggable-elements">
-      <i class="fas fa-cat draggable" draggable="true" id="cat"></i>
-      <i class="fas fa-dog draggable" draggable="true" id="dog"></i>
-      <i class="fas fa-dove draggable" draggable="true" id="dove"></i>
-      <i class="fas fa-fish draggable" draggable="true" id="fish"></i>
-      <i class="fas fa-frog draggable" draggable="true" id="frog"></i>
+    <div class = "quiz_content"> 
+      <h1>Question3</h1>                
+      <h2>How many whiskers does the average cat have on each side of its face?</h2>
+      <div class="image_of_quiz"><img src="images/cat.jpg"></div>
+      <button class="short_options" type="button" onclick="notCorrect()";>A. 1</button>
+      <button class="short_options" type="button" onclick="notCorrect()";>B. 3</button>  
+      <button class="short_options" type="button" onclick="correct()";>C. 12</button>
+      <button class="short_options" type="button" onclick="notCorrect()";>D. 5,007</button>
+    </div>
 
-    </section>
-
-    <!-- this code are refer to https://codepen.io/Coding_Journey/pen/YzKpLvE -->
-    <section class="droppable-elements">
-      <div class="droppable" data-draggable-id="frog"><span>Frog</span></div>
-      <div class="droppable" data-draggable-id="dove"><span>Bird</span></div>
-      <div class="droppable" data-draggable-id="cat"><span>Cat</span></div>
-      <div class="droppable" data-draggable-id="fish"><span>Fish</span></div>
-      <div class="droppable" data-draggable-id="dog"><span>Dog</span></div>
-    </section>
+    <div class = "quiz_content">      
+      <h1>Question4</h1>                     
+      <h2>Why don't birds fall off branches when they sleep?</h2>
+      <button class="options" type="button" onclick="notCorrect()";>A. They are not fully asleep.</button>
+      <button class="options" type="button" onclick="correct()";>B. Their legs lock in place.</button>
+      <button class="options" type="button" onclick="notCorrect()";>C. They perch on wide, flat branches.</button>
+      <button class="options" type="button" onclick="notCorrect()";>D. Their breathing pattern helps them balance.</button>
+    </div>  
+       
+    <a class="prev_question" onclick="plusSlides(-1)">Previous</a>
+    <a class="next_question" onclick="plusSlides(1)">Next</a>
+        
   </section>
-</section>
 
   <?php
     include('footer.php');
